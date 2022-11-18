23aefb620bf14ac5a7f59b2f25dbafc77fb851c6
Josef0801 <92641737+Josef0801@users.noreply.github.com>
2022-11-17 20:27:15 -0600

    Update comments.py

--- augur/api/metrics/comments.py
+++ augur/api/metrics/comments.py
@@ -1,6 +1,46 @@
+#SPDX-License-Identifier: MIT
+"""
+Metrics that provides data about comments in a repository
+"""
 
-def create_routes(app):
+import datetime
+import sqlalchemy as s
+import pandas as pd
+from augur.api.util import register_metric
 
-    @app.route('/{}/comment_lines'.format(AUGUR_API_VERSION), methods=["GET"])
-    def num_languages():
-      pass
+from augur.application.db.engine import create_database_engine
+engine = create_database_engine()
+
+@register_metric()
+def number_of_comment_lines():
+    """Returns total number of issues with comments
+    :param repo_group_id: The repository's repo_group_id
+    :param repo_id: The repository's repo_id, defaults to None
+    :param period: To set the periodicity to 'day', 'week', 'month' or 'year', defaults to 'day'
+    :param begin_date: Specifies the begin date, defaults to '1970-1-1 00:00:00'
+    :param end_date: Specifies the end date, defaults to datetime.now()
+    :return: DataFrame number of total issues
+    """
+    if not begin_date:
+        begin_date = '1970-1-1 00:00:00'
+    if not end_date:
+        end_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    issues_new_SQL = ''
+
+    if not repo_id:
+        issues_new_SQL = s.sql.text("""
+        """)
+
+        results = pd.read_sql(issues_new_SQL, engine, params={'repo_group_id': repo_group_id, 'period': period,
+                                                               'begin_date': begin_date, 'end_date': end_date})
+
+        return results
+
+    else:
+        issues_new_SQL = s.sql.text("""
+        """)
+
+        results = pd.read_sql(issues_new_SQL, engine, params={'repo_id': repo_id, 'period': period,
+                                                               'begin_date': begin_date, 'end_date': end_date})
+        return results
