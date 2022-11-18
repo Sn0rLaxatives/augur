30b16a6da61b29d7386e22730e7520becf41324b
Jreini <99300560+Jreini@users.noreply.github.com>
2022-11-17 19:41:21 -0600

    Update issue.py

--- augur/api/metrics/issue.py
+++ augur/api/metrics/issue.py
@@ -1061,3 +1061,38 @@ def abandoned_issues(repo_group_id, repo_id=None, period='day', begin_date=None,
     results = pd.read_sql(abandonedSQL, engine, params={'repo_id': repo_id, 'repo_group_id': repo_group_id, 'period': period,
                                                                  'begin_date': begin_date, 'end_date': end_date})
     return results
+
+@register_metric()
+def issues_comments_total():
+    """Returns total number of issues with comments
+
+    :param repo_group_id: The repository's repo_group_id
+    :param repo_id: The repository's repo_id, defaults to None
+    :param period: To set the periodicity to 'day', 'week', 'month' or 'year', defaults to 'day'
+    :param begin_date: Specifies the begin date, defaults to '1970-1-1 00:00:00'
+    :param end_date: Specifies the end date, defaults to datetime.now()
+    :return: DataFrame number of total issues
+    """
+    if not begin_date:
+        begin_date = '1970-1-1 00:00:00'
+    if not end_date:
+        end_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    issues_new_SQL = ''
+
+    if not repo_id:
+        issues_new_SQL = s.sql.text("""
+        """)
+
+        results = pd.read_sql(issues_new_SQL, engine, params={'repo_group_id': repo_group_id, 'period': period,
+                                                               'begin_date': begin_date, 'end_date': end_date})
+
+        return results
+
+    else:
+        issues_new_SQL = s.sql.text("""
+        """)
+
+        results = pd.read_sql(issues_new_SQL, engine, params={'repo_id': repo_id, 'period': period,
+                                                               'begin_date': begin_date, 'end_date': end_date})
+        return results
