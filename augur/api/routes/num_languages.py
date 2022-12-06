d5d42131c14b4f863a242cf18983bfb1b8d41b56
Jeffrey Morgan <43151138+Sn0rLaxatives@users.noreply.github.com>
2022-12-06 14:56:53 -0600

    Update num_languages.py
    Added statements to handle repo_id being handed or absent by the user, altered sql statements for better readability

--- augur/api/routes/num_languages.py
+++ augur/api/routes/num_languages.py
@@ -1,12 +1,21 @@
 def create_routes(app):
 
     @app.route('/{}/languages'.format(AUGUR_API_VERSION), methods=["GET"])
-    def num_languages():
-        num_of_languages_sql = s.sql.text("""
-            SELECT COUNT ( DISTINCT programming_language ) AS "Number of Languages"
-            FROM repo_labor 
-            WHERE repo_id = XXXX ;
-        """)
+    def num_languages(repo_id=None):
+        num_of_languages_sql = ""
+        if repo_id is None:
+            num_of_languages_sql = s.sql.text("""
+                SELECT repo_id, COUNT ( DISTINCT programming_language ) AS "Number of Languages"
+                FROM augur_data.repo_labor
+                GROUP BY repo_id;
+            """)
+        else:
+            num_of_languages_sql = s.sql.text("""
+                SELECT repo_id, COUNT ( DISTINCT programming_language ) AS "Number of Languages"
+                FROM augur_data.repo_labor 
+                WHERE repo_id = %d 
+                GROUP BY repo_id;
+            """ %(repo_id))
         results = pd.read_sql(num_of_languages_sql,  server.engine)
         data = results.to_json(orient="records", date_format='iso', date_unit='ms')
         return Response(response=data, status=200, mimetype="application/json")
