d92b9c2b415211d093b456539d50ccf4b01284d8
Jeffrey Morgan <43151138+Sn0rLaxatives@users.noreply.github.com>
2022-12-06 13:00:03 -0600

    Update num_languages.py
    Updated query text, need to verify input route and export route

--- augur/api/routes/num_languages.py
+++ augur/api/routes/num_languages.py
@@ -2,4 +2,13 @@ def create_routes(app):
 
     @app.route('/{}/languages'.format(AUGUR_API_VERSION), methods=["GET"])
     def num_languages():
-      pass
+      num_of_languages_sql = s.sql.text("""
+            SELECT COUNT ( DISTINCT programming_language ) AS "Number of Languages"
+                programming_language,
+                COUNT ( * ) AS language_files 
+            FROM repo_labor 
+            WHERE repo_id = XXXX ;
+        """)
+        results = pd.read_sql(num_of_languages_sql,  server.engine)
+        data = results.to_json(orient="records", date_format='iso', date_unit='ms')
+        return Response(response=data, status=200, mimetype="application/json")
